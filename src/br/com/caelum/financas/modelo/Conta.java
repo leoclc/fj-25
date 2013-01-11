783bd0c3000baee2fff0310e4f351dbff881d187
Leonardo <leoclc@hotmail.com>
2013-01-10 22:59:23 -0200

    Exercicio 8.4

--- src/br/com/caelum/financas/modelo/Conta.java
+++ src/br/com/caelum/financas/modelo/Conta.java
@@ -1,8 +1,11 @@
 package br.com.caelum.financas.modelo;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Conta {
@@ -10,11 +13,14 @@ public class Conta {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	
+	@OneToMany(mappedBy="conta")
+	private List<Movimentacao> movimentacoes;
+	
 	private String titular;
 	private String agencia;
 	private String numero;
 	private String banco;
-
 	public Integer getId() {
 		return id;
 	}
@@ -54,5 +60,14 @@ public class Conta {
 	public void setBanco(String banco) {
 		this.banco = banco;
 	}
+
+	public List<Movimentacao> getMovimentacoes() {
+		return movimentacoes;
+	}
+
+	public void setMovimentacoes(List<Movimentacao> movimentacoes) {
+		this.movimentacoes = movimentacoes;
+	}
+
 
 }
